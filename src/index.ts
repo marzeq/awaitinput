b68b27f90229098a1f7d26fbf01024bf36523af5
marzeq <58303665+marzeq@users.noreply.github.com>
2022-05-02 16:28:18 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import readline from "readline"
+
+export const input = async (question?: string): Promise<string> => {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout
+	})
+
+	return new Promise(resolve => {
+		rl.question(question ?? "", answer => {
+			rl.close()
+			resolve(answer)
+		})
+	})
+}
+
+export default input
